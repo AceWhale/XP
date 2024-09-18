3298984b2e1992b2975905050d3dfde0c76f6d30
NikitA <mohseninikita9@gmail.com>
2024-09-18 19:59:24 +0300

    Code refactoring: tests distributed among classes

--- App/RomanNumber.cs
+++ App/RomanNumber.cs
@@ -6,118 +6,41 @@ using System.Threading.Tasks;
 
 namespace App
 {
-    public class RomanNumber(int Value)
+    public record RomanNumber(int Value)
     {
-        private readonly int _value = Value;
-        public int Value { get { return _value; } }
+        public static RomanNumber Parse(String input) =>
+            RomanNumberParser.FromString(input);
 
-        public static RomanNumber Parse(string Value)
+        public override string? ToString()
         {
-            CheckZeroDigit(Value);
-
-            int result = 0;
-            int rightDigit = 0;      // TODO: rename, 'prev' not semantics
-            int pos = Value.Length;
-            int maxDigit = 0;       // найбільша цифра, що пройдена
-            int lessCounter = 0;    // кількість цифр, менших за неї
-            int maxCounter = 1;     // кількість однакових найбільших цифр
-            foreach (char c in Value.Reverse())
+            Dictionary<int, String> ranges = new()
             {
-                pos--;
-                int digit = ParseDigit(c, pos, Value);
-                ValidateDigitRatio(digit, rightDigit, c, Value, pos);
-                UpdateMaxAndLessCounters(digit, ref maxDigit, ref lessCounter, ref maxCounter);
-                if (IsInvalidLessCounter(lessCounter, maxCounter))
+                { 1, "I" },
+                { 4, "IV" },
+                { 5, "V" },
+                { 9, "IX" },
+                { 10, "X" },
+                { 40, "XL" },
+                { 50, "L" },
+                { 90, "XC" },
+                { 100, "C" },
+                { 400, "CD" },
+                { 500, "D" },
+                { 900, "CM" },
+                { 1000, "M" },
+            };
+            int value = this.Value;
+            if(value == 0) return "N";
+            var result = new StringBuilder();
+            foreach (var pair in ranges.OrderByDescending(kv => kv.Key))
+            {
+                while (value >= pair.Key)
                 {
-                    throw new FormatException(
-                        $"{nameof(RomanNumber)}.{nameof(Parse)}() " +
-                        $"illegal sequence: more than one smaller digits before '{Value[Value.Length - 1]}' in position {Value.Length - 1}");
+                    result.Append(pair.Value);
+                    value -= pair.Key;
                 }
-                result += digit < rightDigit ? -digit : digit;
-                rightDigit = digit;
             }
-            return new(result);
+            return result.ToString();
         }
-
-        public static void CheckZeroDigit(String input)
-        {
-            if(input.Contains('N') && input.Length > 1)
-            {
-                throw new FormatException();
-            }
-        }
-
-        public static bool CheckDigitRatio(int leftDigit, int rightDigit)
-        {
-            //цифри занадто "далекі" для віднімання && віднімання цифр, що є "5"-ками
-            return leftDigit >= rightDigit || !(
-                   rightDigit != 0 && leftDigit != 0 && rightDigit / leftDigit > 10 ||
-                   leftDigit == 5 || leftDigit == 50 || leftDigit == 500
-                );
-        }
-
-        private static int ParseDigit(char c, int pos, string Value)
-        {
-            int digit;
-            try
-            {
-                digit = DigitValue(c);
-            }
-            catch (ArgumentException)
-            {
-                throw new FormatException(
-                    $"{nameof(RomanNumber)}.{nameof(Parse)}()" +
-                    $" found illegal symbol '{c}' in position {pos}");
-            }
-
-            return digit;
-        }
-
-        private static bool IsInvalidLessCounter(int lessCounter, int maxCounter)
-        {
-            return lessCounter > 1 || (lessCounter > 0 && maxCounter > 1);
-        }
-
-        private static void UpdateMaxAndLessCounters(int digit, ref int maxDigit, ref int lessCounter, ref int maxCounter)
-        {
-            if (digit > maxDigit)
-            {
-                maxDigit = digit;
-                lessCounter = 0;
-                maxCounter = 1;
-            }
-            else if (digit == maxDigit)
-            {
-                maxCounter += 1;
-                lessCounter = 0;
-            }
-            else
-            {
-                lessCounter += 1;
-            }
-        }
-
-        private static void ValidateDigitRatio(int digit, int rightDigit, char c, string Value, int pos)
-        {
-            if (!CheckDigitRatio(digit, rightDigit))
-            {
-                throw new FormatException(
-                    $"{nameof(RomanNumber)}.{nameof(Parse)}() " +
-                    $"illegal sequence: '{c}' before '{Value[pos + 1]}' in position {pos}");
-            }
-        }
-
-        public static int DigitValue(char digit) => digit switch
-        {
-            'N' => 0,
-            'I' => 1,
-            'V' => 5,
-            'X' => 10,
-            'L' => 50,
-            'C' => 100,
-            'D' => 500,
-            'M' => 1000,
-            _ => throw new ArgumentException($"{nameof(RomanNumber)}.{nameof(DigitValue)}() ('{digit}')"),
-        };
     }
 }
--- Tests/RomanNumberTest.cs
+++ Tests/RomanNumberTest.cs
@@ -5,6 +5,65 @@ namespace Tests
     [TestClass]
     public class RomanNumberTest
     {
+        private Dictionary<char, int> digits = new()
+        {
+            {'N', 0},
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000},
+        };
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            var testCases = new Dictionary<int, String>()
+            {
+                {4 , "IV"},
+                {6 , "VI"},
+                {19 , "XIX"},
+                {49 , "XLIX"},
+                {95 , "XCV"},
+                {444 , "CDXLIV"},
+                {946 , "CMXLVI"},
+                {3333 , "MMMCCCXXXIII"},
+            }
+            .Concat(digits.Select(d => new
+                KeyValuePair<int, string>(d.Value, d.Key.ToString()))
+            )
+            .ToDictionary();
+            foreach (var testCase in testCases)
+            {
+                RomanNumber rn = new(testCase.Key);
+                var value = rn.ToString();
+                Assert.IsNotNull(value);
+                Assert.AreEqual(testCase.Value, value);
+            }
+        }
+
+        [TestMethod]
+        public void CrossTest_Parse_ToString()
+        {
+            // Наявність двох методів протилежної роботи дозволяє
+            // використовувати крос-тести, які послідовно застосовують
+            // два методи і одержують початковий результат
+            // "XIX" -Parse-> 19 -ToString-> "XIX"  v
+            // "IIII" -Parse-> 19 -ToString-> "IV"  x
+            // 4 -ToString-> "IV -Parse-> 4     v
+            for (int i = 0; i <= 1000; i++)
+            {
+                int c = RomanNumberParser.FromString(new RomanNumber(i).ToString()!).Value;
+                Assert.AreEqual(
+                    i,
+                    c,
+                    $"Cross test for {i}: {new RomanNumber(i)} -> {c}"
+                );
+            }
+        }
+
         [TestMethod]
         public void ParseTest()
         {
@@ -58,7 +117,7 @@ namespace Tests
             foreach (var exCase in exCases)
             {
                 var ex = Assert.ThrowsException<FormatException>(
-                    () => RomanNumber.Parse(exCase[0].ToString()),
+                    () => RomanNumber.Parse(exCase[0].ToString()!),
                     $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
                     );
                 // накладаємо вимоги на повідомлення
@@ -80,130 +139,15 @@ namespace Tests
                     );
             }
 
-            /* Тести на неправильну композицію числа (всі цифри правильні,
-             * але неправильна їх послідовність)
-             * VV, VX, IIX, ...
-             */
-            Object[][] exCases2 = [
-                ["VX", "V", "X", 0],  // ---
-                ["LC", "L", "C", 0],  // "відстань" між цифрами при відніманні:
-                ["DM", "D", "M", 0],  // відніматись можуть I, X, СI причому від
-                ["IC", "I", "C", 0],  // двох сусідніх цифр (I - від V та X, ... )
-                ["MIM", "I", "M", 1],
-                ["MVM", "V", "M", 1],
-                ["MXM", "X", "M", 1],
-                ["CVC", "V", "C", 1],
-                ["MCVC", "V", "C", 2],
-                ["DCIC", "I", "C", 2],
-                ["IM", "I", "M", 0],
-            ];
-            foreach (var exCase in exCases2)
-            {
-                var ex = Assert.ThrowsException<FormatException>(
-                    () => RomanNumber.Parse(exCase[0].ToString()),
-                    $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
-                    );
-                Assert.IsTrue(
-                    ex.Message.Contains($"illegal sequence: '{exCase[1]}' before '{exCase[2]}'"),
-                    $"ex.Message must contain symbols which cause error: '{exCase[1]}' and '{exCase[2]}', testCase: '{exCase[0]}', ex.Message: {ex.Message}"
-                    );
-                Assert.IsTrue(
-                    ex.Message.Contains($"in position {exCase[3]}"),
-                    $"ex.Message must contain error symbol position, testCase: '{exCase[0]}'"
-                    );
-                Assert.IsTrue(
-                    ex.Message.Contains(nameof(RomanNumber)) &&
-                    ex.Message.Contains(nameof(RomanNumber)),
-                    $"ex.Message must contain names of class and method, testCase: '{exCase[0]}', ex.Message: {ex.Message}"
-                    );
-            }
 
+            
 
-
-            Object[][] exCases3 = [
-                [ "IIX", 'X', 2 ],   // Перед цифрою є декілька цифр, менших за неї
-                [ "VIX", 'X', 2 ],   // !! кожна пара цифр - правильна комбінація,
-                [ "XXC", 'C', 2 ],   //    проблема створюється щонайменше трьома цифрами
-                [ "IXC", 'C', 2 ],   
-                [ "IIIV", 'V', 3]   
-            ];
-            foreach (var exCase in exCases3)
-            {
-                var ex = Assert.ThrowsException<FormatException>(
-                    () => RomanNumber.Parse(exCase[0].ToString()),
-                    $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
-                    );
-                Assert.IsTrue(
-                    ex.Message.Contains($"illegal sequence: more than one smaller digits before '{exCase[1]}'"),
-                    $"ex.Message must contain symbol before error: '{exCase[1]}', testCase: '{exCase[0]}', ex.Message: {ex.Message}"
-                    );
-                Assert.IsTrue(
-                    ex.Message.Contains($"in position {exCase[2]}"),
-                    $"ex.Message must contain error symbol position, testCase: '{exCase[0]}'"
-                    );
-                Assert.IsTrue(
-                    ex.Message.Contains(nameof(RomanNumber)) &&
-                    ex.Message.Contains(nameof(RomanNumber)),
-                    $"ex.Message must contain names of class and method, testCase: '{exCase[0]}', ex.Message: {ex.Message}"
-                    );
-            }
-
-            Object[][] exCases4 = [
-                [ "IXX", 'I', 0 ],
-                [ "IXXX", 'I', 0],
-                [ "XCC", 'X', 0 ],
-                [ "XCCC", 'X', 0],
-                [ "CXCC", 'X', 1],
-                [ "CMM", 'C', 0 ],
-                [ "CMMM", 'C', 0],
-                [ "MCMM", 'C', 1],
-                [ "LCC", 'L', 0 ],
-                [ "ICCC", 'I', 0]
-            ];
-            foreach (var exCase in exCases4)
-            {
-                var ex = Assert.ThrowsException<FormatException>(
-                    () => RomanNumber.Parse(exCase[0].ToString()),
-                    $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
-                    );
-            }
-
-
-            Object[][] exCases5 = [
-                [ "NN",   '0', 1 ],   // Цифра N не може бути у числі, тільки
-                [ "IN",   '1', 1 ],   // сама по собі
-                [ "NX",   '0', 0 ],   
-                [ "NC",   '1', 1 ],   
-                [ "XNC",  '1', 1 ],   
-                [ "XVIN", '3', 3 ],   
-                [ "XNNC", '1', 1 ],   
-                [ "NMC",  '1', 1 ],   
-                [ "NIX",  '1', 1 ]
-            ];
-            foreach (var exCase in exCases5)
-            {
-                var ex = Assert.ThrowsException<FormatException>(
-                    () => RomanNumber.Parse(exCase[0].ToString()),
-                    $"RomanNumber.Parse(\"{exCase[0]}\") must throw FormatException"
-                    );
-            }
         }
 
         [TestMethod]
         public void DigitValueChar()
         {
-            //Dictionary<char, int> testCases = new()
-            //{
-            //    {'N', 0},
-            //    {'I', 1},
-            //    {'V', 5},
-            //    {'X', 10},
-            //    {'L', 50},
-            //    {'C', 100},
-            //    {'D', 500},
-            //    {'M', 1000},
-            //};
-            //foreach (var testCase in testCases)
+            //foreach (var testCase in digits)
             //{
             //    Assert.AreEqual(testCase.Value,
             //        RomanNumber.DigitValue(testCase.Key),
@@ -239,7 +183,7 @@ namespace Tests
             foreach (var digit in excCases)
             {
                 var ex = Assert.ThrowsException<ArgumentException>(
-                () => RomanNumber.DigitValue(digit),
+                () => RomanNumberParser.DigitValue(digit),
                 $"DigitValue({digit}) must throw ArgumentException"
                 );
                 Assert.IsTrue(
@@ -249,7 +193,7 @@ namespace Tests
                     );
                 Assert.IsTrue(
                     ex.Message.Contains($"{nameof(RomanNumber)}") &&
-                    ex.Message.Contains($"{nameof(RomanNumber.DigitValue)}"),
+                    ex.Message.Contains($"DigitValue"),
                     "Not valid Roman digit:" +
                     $" argument: ('{digit}'), ex.Message: {ex.Message}"
                     );
